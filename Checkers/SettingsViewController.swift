690aa7153bce0a7b97862c931ce8aafb038cc857
ElAlexandra <korolevanosale@gmail.com>
2021-08-03 16:37:38 +0300

    checkers

---
+++ Checkers/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  Checkers
+//
+//  Created by Александра  Значёнок on 26.07.21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBAction func menuButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+     
+
+}
